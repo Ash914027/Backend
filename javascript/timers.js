7554f7cb1f61f45440938ccd5398c56371d41f66
Ashish Chauhan <119170180+Ash914027@users.noreply.github.com>
2024-04-18 00:17:00 +0530

    Update timers.js

--- javascript/timers.js
+++ javascript/timers.js
@@ -1,11 +1,11 @@
 // Timers in JS 
 
 // setTimeout function
-// const delayedFunction = () =>
-// {
-//     console.log(`Delayed function executed!`);
-// }
-// setTimeout(delayedFunction,2000);  // Executes after 2 Seconds
+const delayedFunction = () =>
+{
+    console.log(`Delayed function executed!`);
+}
+setTimeout(delayedFunction,2000);  // Executes after 2 Seconds
 
 
 // setInterval function
