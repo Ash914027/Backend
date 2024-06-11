d737ad6b05404c9db88db5d25e3214b7b47129a0
Ashish Chauhan <119170180+Ash914027@users.noreply.github.com>
2024-06-11 19:37:02 +0530

    Update app.js

--- mongodb_database/app.js
+++ mongodb_database/app.js
@@ -1,30 +1,38 @@
+// Import the required modules
 const express = require("express");
 const mongoose = require("mongoose");
 
+// Create an Express app instance
 const app = express();
 const port = 4000;
 
+// Use the JSON middleware to parse incoming requests
 app.use(express.json());
 
+// Connect to the MongoDB database
 mongoose
   .connect("mongodb://127.0.0.1:27017/user_management_db")
   .then(() => console.log("connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
+// Define the schema for the User model
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
 });
 
+// Create the User model
 const User = mongoose.model("User", userSchema);
 
+// Route to get all users
 app.get("/users", (req, res) => {
   User.find({})
     .then((users) => res.json(users))
     .catch((err) => res.status(500).json({ message: err.message }));
 });
 
+// Route to create a new user
 app.post("/users", (req, res) => {
   const user = new User({
     name: req.body.name,
@@ -37,6 +45,7 @@ app.post("/users", (req, res) => {
     .catch((err) => res.status(400).json({ message: err.message }));
 });
 
+// Route to update a user by ID
 app.put("/users/:id", (req, res) => {
   const userId = req.params.id;
   const updateData = {
@@ -53,6 +62,8 @@ app.put("/users/:id", (req, res) => {
     })
     .catch((err) => res.status(400).json({ message: err.message }));
 });
+
+// Route to delete a user by ID
 app.delete("/users/:id", (req, res) => {
   const userId = req.params.id;
   User.findByIdAndRemove(userId)
@@ -64,6 +75,8 @@ app.delete("/users/:id", (req, res) => {
     })
     .catch((err) => res.status(400).json({ message: err.message }));
 });
+
+// Start the server and listen for requests
 app.listen(port, () => {
   console.log("server is live http://localhost:4000");
 });
