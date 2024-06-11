d819bb0fff8ea850e3a63d83f052487b9b59f6f1
Ashish Chauhan <119170180+Ash914027@users.noreply.github.com>
2024-06-11 19:37:58 +0530

    Update buffer.js

--- nodejs/buffer.js
+++ nodejs/buffer.js
@@ -3,15 +3,15 @@ const buffer=Buffer.alloc(4)
 // console.log(buffer);
 
 // //writing data to buffer
-// buffer.write('Hello','utf-8');
-// // console.log(buffer);
+buffer.write('Hello','utf-8');
+console.log(buffer);
 
 // // //reading from buffer
-// const data =buffer.toString('utf-8');
-// console.log(data);
+const data =buffer.toString('utf-8');
+console.log(data);
 
 ////concatenating buffer
-// const buffer1=Buffer.from('Hello', 'utf-8');
-// const buffer2=Buffer.from('World', 'utf-8');
-// const buffer3=Buffer.concat([buffer1,buffer2]);
-// console.log(buffer3.toString('utf-8'));
+const buffer1=Buffer.from('Hello', 'utf-8');
+const buffer2=Buffer.from('World', 'utf-8');
+const buffer3=Buffer.concat([buffer1,buffer2]);
+console.log(buffer3.toString('utf-8'));
