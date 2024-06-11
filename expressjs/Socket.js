e9947a0efef168aa9423a756538b308322718089
Ashish Chauhan <119170180+Ash914027@users.noreply.github.com>
2024-06-11 19:33:24 +0530

    Update Socket.js

--- expressjs/Socket.js
+++ expressjs/Socket.js
@@ -9,3 +9,33 @@
 const express=require('express');
 const SocketIO=require('socket.io');
 const path=require('path');
+// server.js
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Handle socket connections
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  // Handle chat messages
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  // Handle disconnections
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
